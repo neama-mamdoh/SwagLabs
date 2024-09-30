1a04e1f686b65162a03fbd50241ac86e85b91eb2
naema-mamdoh <nemomamdoh02@gmail.com>
2024-10-01 00:54:06 +0300

    Add To Cart Changes num 1

--- src/main/java/org/example/ProductsPage.java
+++ src/main/java/org/example/ProductsPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 public class ProductsPage{
     private WebDriver driver;
     private By asserMsg = By.cssSelector("span[data-test='title']");
+    private By addBackPack = By.id("add-to-cart-sauce-labs-backpack");
     public ProductsPage(WebDriver driver) {
         this.driver = driver;
     }
@@ -14,4 +15,7 @@ public class ProductsPage{
         return driver.findElement(asserMsg);
     }
 
+    public void addBBackackToCart(){
+        driver.findElement(addBackPack).click();
+    }
 }
